540452af71a51b97c07b4d40ea8b28e7fc6c4064
studiogroenrijk <77233246+studiogroenrijk@users.noreply.github.com>
2021-01-18 15:14:48 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 simpleCart({
-    checkout: { 
-        type: "SendForm" , 
-        url: "http://example.com/your/custom/checkout/url" 
-    } 
+    checkout: {
+        type: "SendForm" ,
+        url: "http://example.com/your/custom/checkout/url" ,
+        method: "GET" ,
+        success: "success.html" ,
+        cancel: "cancel.html" ,
+        extra_data: {
+            customInfo1: "some custom information",
+            customInfo2: "12321321"
+        }
+    }
 });
 
 simpleCart.currency( "EUR" ); // set the currency to Euros

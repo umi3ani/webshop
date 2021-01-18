51de6bacc1076eee53de5acc54a8dfc91ba7835f
studiogroenrijk <77233246+studiogroenrijk@users.noreply.github.com>
2021-01-18 16:48:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,8 @@
 simpleCart({
-    // array representing the format and columns of the cart,
-    // see the cart columns documentation
-    cartColumns: [
-        { attr: "name", label: "Name"},
-        { view: "currency", attr: "price", label: "Price"},
-        { view: "decrement", label: false},
-        { attr: "quantity", label: "Qty"},
-        { view: "increment", label: false},
-        { view: "currency", attr: "total", label: "SubTotal" },
-        { view: "remove", text: "Remove", label: false}
-    ],
-    // "div" or "table" - builds the cart as a 
-    // table or collection of divs
-    cartStyle: "div", 
-    // how simpleCart should checkout, see the 
-    // checkout reference for more info 
-    checkout: {
-        type: "SendForm" ,
-        url: "https://studiogroenrijk.github.io/webshop/verzenden/" ,
-        method: "GET" ,
-        success: "success.html" ,
-        cancel: "cancel.html" ,
-        extra_data: {
-            customInfo1: "some custom information",
-            customInfo2: "12321321"
-        }
-    },
-    // collection of arbitrary data you may want to store 
-    // with the cart, such as customer info
-    data: {},
-    // set the cart langauge 
-    // (may be used for checkout)
-    language: "english-us",
-    // array of item fields that will not be 
-    // sent to checkout
-    excludeFromCheckout: [],
-    // custom function to add shipping cost
-    shippingCustom: null,
-    // flat rate shipping option
-    shippingFlatRate: 0,
-    // added shipping based on this value 
-    // multiplied by the cart quantity
-    shippingQuantityRate: 0,
-    // added shipping based on this value 
-    // multiplied by the cart subtotal
-    shippingTotalRate: 0,
-    // tax rate applied to cart subtotal
-    taxRate: 0,
-    // true if tax should be applied to shipping
-    taxShipping: false,
-    // event callbacks 
-    beforeAdd            : null,
-    afterAdd            : null,
-    load                : null,
-    beforeSave        : null,
-    afterSave            : null,
-    update            : null,
-    ready            : null,
-    checkoutSuccess    : null,
-    checkoutFail        : null,
-    beforeCheckout        : null,
-        beforeRemove           : null
+  checkout: {
+    type: "SendForm",
+    email: "studio@groenrijk.nl"
+  }
 });
 
     // Configure EURO as currency
@@ -73,7 +15,6 @@ simpleCart({
       after: false,
       accuracy: 2
     });
-
 
 //* Add shopping cart dropdown in header
 jQuery(document).ready(function () {

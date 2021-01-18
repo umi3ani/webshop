0366e69af6845a37334b3087284ddb92cf5b1e31
studiogroenrijk <77233246+studiogroenrijk@users.noreply.github.com>
2021-01-18 14:40:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
 //* Add PayPal Email
 simpleCart({
-        checkout: { 
-        type: "SendForm" , 
-        url: "http://example.com/your/custom/checkout/url" ,
-        // http method for form, "POST" or "GET", default is "POST"
-        method: "GET" , 
-        // url to return to on successful checkout, default is null
-        success: "success.html" , 
-        // url to return to on cancelled checkout, default is null
-        cancel: "cancel.html" ,
-        // an option list of extra name/value pairs that can
-        // be sent along with the checkout data
-        extra_data: {
-        storename: "Bob's cool plumbing store",
-        cartid: "12321321"
-        }
-    } 
-                currency: "EUR"
+    currency: "GBP" // set the currency to pounds sterling
 });
 
 //* Add shopping cart dropdown in header

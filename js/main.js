8bb6b44bd81ec3f3c64060bbd74ff71bdbf96bd0
studiogroenrijk <77233246+studiogroenrijk@users.noreply.github.com>
2021-01-18 17:06:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,55 @@
 simpleCart({
-  checkout: {
-    type: "SendForm",
-    email: "studio@groenrijk.nl"
-  }
+    checkout: {
+        type: "SendForm" ,
+        url: "https://studiogroenrijk.github.io/webshop/verzenden/" ,
+        method: "GET" ,
+        success: "success.html" ,
+        cancel: "cancel.html" ,
+        extra_data: {
+        customInfo1: "some custom information",
+        customInfo2: "12321321"
+        }
+    },
+    // set the currency, see the currency 
+    // reference for more info
+    currency: "EUR",
+    // collection of arbitrary data you may want to store 
+    // with the cart, such as customer info
+    data: {},
+    // set the cart langauge 
+    // (may be used for checkout)
+    language: "english-us",
+    // array of item fields that will not be 
+    // sent to checkout
+    excludeFromCheckout: [],
+    // custom function to add shipping cost
+    shippingCustom: null,
+    // flat rate shipping option
+    shippingFlatRate: 0,
+    // added shipping based on this value 
+    // multiplied by the cart quantity
+    shippingQuantityRate: 0,
+    // added shipping based on this value 
+    // multiplied by the cart subtotal
+    shippingTotalRate: 0,
+    // tax rate applied to cart subtotal
+    taxRate: 0,
+    // true if tax should be applied to shipping
+    taxShipping: false,
+    // event callbacks 
+    beforeAdd            : null,
+    afterAdd            : null,
+    load                : null,
+    beforeSave        : null,
+    afterSave            : null,
+    update            : null,
+    ready            : null,
+    checkoutSuccess    : null,
+    checkoutFail        : null,
+    beforeCheckout        : null,
+    beforeRemove           : null
 });
 
-    // Configure EURO as currency
-    simpleCart.currency({
-      code: "EUR",
-      name: "Euro",
-      symbol: " &euro; ",
-      delimiter: ".",
-      decimal: ",",
-      after: false,
-      accuracy: 2
-    });
 
 //* Add shopping cart dropdown in header
 jQuery(document).ready(function () {

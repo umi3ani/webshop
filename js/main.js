35617c379fb60adb73a5c13a7a6d2a1c4a4afafe
studiogroenrijk <77233246+studiogroenrijk@users.noreply.github.com>
2021-01-18 16:40:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,70 +1,30 @@
-simpleCart({
-    // array representing the format and columns of the cart,
-    // see the cart columns documentation
-    cartColumns: [
-        { attr: "name", label: "Name"},
-        { view: "currency", attr: "price", label: "Price"},
-        { view: "decrement", label: false},
-        { attr: "quantity", label: "Qty"},
-        { view: "increment", label: false},
-        { view: "currency", attr: "total", label: "SubTotal" },
-        { view: "remove", text: "Remove", label: false}
-    ],
-    // "div" or "table" - builds the cart as a 
-    // table or collection of divs
-    cartStyle: "div", 
-    // how simpleCart should checkout, see the 
-    // checkout reference for more info 
-    checkout: {
-        type: "SendForm" ,
-        url: "https://studiogroenrijk.github.io/webshop/verzenden/" ,
-        method: "GET" ,
-        success: "success.html" ,
-        cancel: "cancel.html" ,
+    simpleCart({
+      cartColumns: [
+        {attr: "name", label: "Name"},
+        {attr: "price", label: "Preis", view: 'currency'},
+        {attr: "quantity", label: "Menge"},
+        {attr: "total", label: "Summe", view: 'currency'},
+        {view: "remove", text: "Remove", label: false}
+      ],
+      checkout: {
+        type: "SendForm",
+        url: "https://secure.fundraisingbox.com/app/simpleCartJs",
         extra_data: {
-            customInfo1: "some custom information",
-            customInfo2: "12321321"
+          hash: "PUT_YOUR_FORM_HASH_HERE" // <-- TODO
         }
-    },
-    // set the currency, see the currency 
-    // reference for more info
-    currency: "USD",
-    // collection of arbitrary data you may want to store 
-    // with the cart, such as customer info
-    data: {},
-    // set the cart langauge 
-    // (may be used for checkout)
-    language: "english-us",
-    // array of item fields that will not be 
-    // sent to checkout
-    excludeFromCheckout: [],
-    // custom function to add shipping cost
-    shippingCustom: null,
-    // flat rate shipping option
-    shippingFlatRate: 0,
-    // added shipping based on this value 
-    // multiplied by the cart quantity
-    shippingQuantityRate: 0,
-    // added shipping based on this value 
-    // multiplied by the cart subtotal
-    shippingTotalRate: 0,
-    // tax rate applied to cart subtotal
-    taxRate: 0,
-    // true if tax should be applied to shipping
-    taxShipping: false,
-    // event callbacks 
-    beforeAdd            : null,
-    afterAdd            : null,
-    load                : null,
-    beforeSave        : null,
-    afterSave            : null,
-    update            : null,
-    ready            : null,
-    checkoutSuccess    : null,
-    checkoutFail        : null,
-    beforeCheckout        : null,
-        beforeRemove           : null
-});
+      }
+    });
+
+    // Configure EURO as currency
+    simpleCart.currency({
+      code: "EUR",
+      name: "Euro",
+      symbol: " &euro; ",
+      delimiter: ".",
+      decimal: ",",
+      after: false,
+      accuracy: 2
+    });
 
 simpleCart.currency( "EUR" ); // set the currency to Euros
 

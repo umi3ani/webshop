de57b39f911efb01e86bb90ec3a7c61d1c0f4464
studiogroenrijk <77233246+studiogroenrijk@users.noreply.github.com>
2021-01-26 22:35:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -88,6 +88,17 @@ simpleCart.shipping(function(){
          return 6.95; // amount for > 500
     }
 });
+	
+	
+simpleCart.shipping = function(){
+  if( ("shippingSelect").val() == "de" ){return 29;}
+  if( ("shippingSelect").val() == "at" ){return 49;}
+  if( ("shippingSelect").val() == "eu" ){return 99;}
+  if( ("shippingSelect").val() == "ch" ){return 149;}
+  if( ("shippingSelect").val() == "welt" ){return 249;}
+};
+
+	
 	simpleStore.init({
 
 		// brand can be text or image URL
